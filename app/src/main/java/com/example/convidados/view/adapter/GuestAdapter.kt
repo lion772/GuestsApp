3e3fbe4483d0293dc52d486a879728b3e705caa0
netpos <wmello@netpos.com.br>
2020-07-05 22:17:08 -0300

    adicionando xml row_guest e preenchendo o GuestAdapter e o GuestViewHolder

--- app/src/main/java/com/example/convidados/view/adapter/GuestAdapter.kt
+++ app/src/main/java/com/example/convidados/view/adapter/GuestAdapter.kt
@@ -1,22 +1,35 @@
 package com.example.convidados.view.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Adapter
 import androidx.recyclerview.widget.RecyclerView
+import com.example.convidados.R
+import com.example.convidados.service.model.GuestModel
 import com.example.convidados.view.viewholder.GuestViewHolder
 
 class GuestAdapter: RecyclerView.Adapter<GuestViewHolder>() {
 
+    private var _guestList: List<GuestModel> = arrayListOf()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GuestViewHolder {
-        TODO("Not yet implemented")
+        val item = LayoutInflater.from(parent.context).inflate(R.layout.row_guest, parent, false )
+        return GuestViewHolder(item)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return _guestList.count()
     }
 
     override fun onBindViewHolder(holder: GuestViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(_guestList[position])
+
+
+    }
+
+    fun update(list: List<GuestModel>){
+
+        _guestList = list
     }
 
 
--- app/src/main/java/com/example/convidados/view/viewholder/GuestViewHolder.kt
+++ app/src/main/java/com/example/convidados/view/viewholder/GuestViewHolder.kt
@@ -1,9 +1,21 @@
 package com.example.convidados.view.viewholder
 
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.convidados.R
+import com.example.convidados.service.model.GuestModel
+import kotlinx.android.synthetic.main.row_guest.view.*
 
 /* ViewHolder é uma classe que guarda as referências dos elementos de layout */
 
 class GuestViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    fun bind(guest: GuestModel) {
+
+        val textName = itemView.findViewById<TextView>(R.id.text_name)
+        textName.text = guest.name
+    }
+
+
 }

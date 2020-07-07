24b671393dab036c60e1802aaa1af103ebce26e5
netpos <wmello@netpos.com.br>
2020-07-06 21:12:32 -0300

    Ajeitando o ciclo de vida da activity

--- app/src/main/java/com/example/convidados/view/viewholder/GuestViewHolder.kt
+++ app/src/main/java/com/example/convidados/view/viewholder/GuestViewHolder.kt
@@ -12,7 +12,6 @@ import kotlinx.android.synthetic.main.row_guest.view.*
 class GuestViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     fun bind(guest: GuestModel) {
-
         val textName = itemView.findViewById<TextView>(R.id.text_name)
         textName.text = guest.name
     }
